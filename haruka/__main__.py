5b5823bbe16d4060270e0020cc834447ff422af1
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-09-04 15:58:57 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -24,24 +24,18 @@ PM_START = """Hello {}, my name is {}!
 
 You know how hard it is sometimes to manage group so here is the solution for you
 
-I'm group manager bot and Anti-spam for RR Players🃏
+I'm group Silvermist bot and Anti-spam.
 
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-My owner is [Aditya 🇮🇳](t.me/xditya)
+My owner is [Dark] (t.me/userge_dark)
 
-Special thanks to [this person](t.me/denver02) for helping me.
+Special thanks to [this person](t.me/demogod_1) for helping me.
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
 Click /help or Help button below to find out more about how to use me to my full potential.
-
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
 """
-
 
 IMPORTED = {}
 MIGRATEABLE = []
