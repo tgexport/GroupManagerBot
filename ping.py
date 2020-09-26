4c91efb519001ea20339846af4f62c12479bfe69
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-09-26 20:09:02 +0530

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,5 +1,7 @@
-  "ping
-  Info {send ping Status}
-
-   "pingall"
-   Info {send all ping Status}
+"ping", about={'header': "check how long it takes to ping your userbot"})
+async def pingme(message: Message):
+    start = datetime.now()
+    await message.edit('`Pong!`')
+    end = datetime.now()
+    m_s = (end - start).microseconds / 1000
+    await message.edit(f"**Pong!**\n`{m_s} ms`")
