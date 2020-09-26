9ba128b989296c24b6e3c8907af3fba1c60e4599
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-09-26 20:59:42 +0530

    Create ping.py

---
+++ haruka/modules/ping.py
@@ -0,0 +1,11 @@
+
+
+
+@groupmannegment.on_cmd(
+    "ping", about={'header': "check how long it takes to ping your userbot"})
+async def pingme(message: Message):
+    start = datetime.now()
+    await message.edit('`Pong!`')
+    end = datetime.now()
+    m_s = (end - start).microseconds / 1000
+    await message.edit(f"**Pong!**\n`{m_s} ms`")
