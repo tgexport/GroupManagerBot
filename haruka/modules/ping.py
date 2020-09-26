26a0fef6ebd3d96e84cedcf6021c79b2cd1e5880
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-09-26 20:26:49 +0530

    Update ping.py

--- haruka/modules/ping.py
+++ haruka/modules/ping.py
@@ -1,4 +1,17 @@
-"ping", about={'header': "check how long it takes to ping your userbot"})
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from datetime import datetime
+from userge import userge, Message
+
+
+@userge.on_cmd(
+    "ping", about={'header': "check how long it takes to ping your userbot"})
 async def pingme(message: Message):
     start = datetime.now()
     await message.edit('`Pong!`')
