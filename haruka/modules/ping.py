123c7daa721e5e5543179b9ed39c971808166299
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-09-27 10:39:02 +0530

    Update  ping.py

--- haruka/modules/ping.py
+++ haruka/modules/ping.py
@@ -1,10 +1,10 @@
 
 
-@dark12knight.on_cmd(
-    "ping", about={'header': "check how long it takes to ping your bot"})
+@run_async.on_cmd(
+    "/ping", about={'header': "check how long it takes to ping your bot"})
 async def pingme(message: Message):
     start = datetime.now()
     await message.edit('Pong!')
     end = datetime.now()
     m_s = (end - start).microseconds / 1000
-    await message.edit(f"**Pong!**\n{m_s} ms")
+    await message.edit💎(f"**Pong!**\n{m_s} ms")💎
