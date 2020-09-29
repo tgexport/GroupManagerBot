2b4444f808453a83f575fc98da1c6120d516c2f2
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-09-29 21:13:05 +0530

    Create ping.py

---
+++ haruka/modules/ping.py
@@ -0,0 +1,9 @@
+
+
+telegram.on_cmd(
+    "!ping", about={'header': " def pingme(message: Message):
+    start = datetime.now()
+    await message.edit('Pong!')
+    end = datetime.now()
+    m_s = (end - start).microseconds / 1000
+    await message.edit💎(f"**Pong!**\n{m_s} ms ✅")
