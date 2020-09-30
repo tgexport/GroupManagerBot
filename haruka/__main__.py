201c8415467e1db1d0aa5a0121af2c86df5336ea
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-09-30 17:50:23 +0530

    Update __main__.oy

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -143,7 +143,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             send_start(bot, update)
     else:
-        update.effective_message.reply_text("I'm Online 🟢")
+        update.effective_message.reply_text("I'm Online")
 
 def send_start(bot, update):
     #Try to remove old message
