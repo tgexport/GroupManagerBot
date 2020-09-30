5bec06b059327fc81679f3fe311900c01aa1d33f
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-09-30 14:57:39 +0530

    Delete ping.py

--- haruka/modules/ping.py
+++
@@ -1,9 +0,0 @@
-
-
-.on_cmd(
-    "!ping", about={'header': " def pingme(message: Message):
-    start = datetime.now()
-    await message.edit('Pong!')
-    end = datetime.now()
-    m_s = (end - start).microseconds / 1000
-    await message.edit💎(f"**Pong!**\n{m_s} ms ✅")
