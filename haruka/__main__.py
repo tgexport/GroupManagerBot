6795977fd17f499019c2c240855533d64a1deee2
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-10-03 12:02:17 +0530

    Update __main__.py
    Testing ping

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -140,8 +140,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 control_panel(bot, update)
     else: 
         send_ping(bot,update)
-        else: 
-           update.effective_message.reply_text("pong")                 
+          update.effective_message.reply_text("pong")                 
 def pingme(message: Message):  
     start = datetime.now()
     a = await message.reply('Pong!')
