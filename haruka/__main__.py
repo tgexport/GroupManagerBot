a97e5a65446571e266765577a4819329e68fe96f
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-10-04 10:01:24 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -138,8 +138,8 @@ def start(bot: Bot, update: Update, args: List[str]):
 
             elif args[0].lower() == "controlpanel":
                 control_panel(bot, update)
-         else:
-            send_start(bot, update)
+          else:
+             send_start(bot, update)
      else:
         update.effective_message.reply_text("☮️I'm Online💐")
 
